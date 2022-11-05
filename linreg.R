a1edbc1434c23aa06262559d695039b28755494b
Julius Wirbel <jwirbel1@outlook.com>
2022-11-05 15:20:46 +0100

    added linear regression file

---
+++ linreg.R
@@ -0,0 +1,7 @@
+install.packages("performance")
+install.packages("see")
+install.packages("visreg")
+library("tidyverse", "see", "performance", "visreg")
+
+mean_obesity = read_csv()
+ob.lm <- lm(obesity ~ )
